98552d94bfd099ebe5251cf26e667111c4e5abc5
Sarthak <sarthakmjh@gmail.com>
2024-02-08 15:45:56 +0530

    babel,jsx

--- 03/app.js
+++ 03/app.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// React Element
 
-const heading= React.createElement("div",{id:'parent'},
-    [React.createElement("div",{id:"child1"},
-    [React.createElement("h1",{},'Hello i am h1 tag'),React.createElement("h2",{},'Hello i am h2 tag')]),
-    React.createElement("div",{id:"child2"},
-    [React.createElement("h1",{},'Hello i am h1 tag'),React.createElement("h2",{},'Hello i am h2 tag')])
-])
+const heading = React.createElement("h1",{id:"heading"},"Hello React")
+// create element is old way of doing this
 
 
+// JSX(transpiled before it reaches the JS)-Done by Parcel-By using Babel. 
 
-const root=ReactDOM.createRoot(document.getElementById('root'))
+// const jsxHeading= (<h1 id="heading">
+//     Namste react using JSX
+//     </h1>) // if we want to write babel in multiple line, we need to use () brackets
 
-root.render(heading)
+
+const jsxHeading=<img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"/>
+
+// JSX is not html indside JS. It have XML, HTML  like syntax.
+
+// React Components
+// ClassBased COmponents -Old Way of writting code
+//Functional Components- New way of writting code
+
+//react functional componets: It is a Simple javaScript Function
+
+const HeadingComponent=()=>(
+    <div>
+        <h1>React Functional Component</h1>
+    </div>
+)
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<HeadingComponent />)
+
+
+
+// --Babel: It takes js and converts it to the code that js engine can understand. Transpiler, javascript compiler. Babel transpiles code to a code that old browsers can even understand. 
+
+// React element reffers to the js code and reac t functional component returns react element
