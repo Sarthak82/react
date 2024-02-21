7d080f78a4b1a42c6a145e209e0e5e892971b186
Sarthak <sarthakmjh@gmail.com>
2024-02-21 16:32:17 +0530

    Some minnor updates

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,15 +1,6 @@
-// import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './components/Header';
-import Body from './components/Body'
-const AppLayoutComponet=()=>{
-    return (
-        <div className="app">
-            <Header />
-            <Body />
-        </div>
-    )
-}
+import AppLayoutComponet from './components/AppLayoutComponent';
+
 
 
 const root=ReactDOM.createRoot(document.getElementById('root'))
